79b34238daba16312e88fd76466231bbd3f04c68
wslyvh <wslyvh@users.noreply.github.com>
2020-03-28 15:36:37 +0100

    trade handler

--- src/account.ts
+++ src/account.ts
@@ -1,6 +1,17 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 import { IHandlerResponse } from "./types";
 
+export const trade: APIGatewayProxyHandler = async () => {
+  console.log("EXEC account.trade");
+
+  const response: IHandlerResponse = {
+    statusCode: 200,
+    body: "Success. Account Trade"
+  };
+
+  return response;
+};
+
 export const withdraw: APIGatewayProxyHandler = async () => {
   console.log("EXEC account.withdraw");
 
