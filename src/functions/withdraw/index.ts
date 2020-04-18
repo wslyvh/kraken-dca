22ba0a098e2b163f4d84e4408ab7c4ad6e193caa
wslyvh <wslyvh@users.noreply.github.com>
2020-04-18 15:58:09 +0200

    functions

---
+++ src/functions/withdraw/index.ts
@@ -0,0 +1,12 @@
+import { AzureFunction, Context } from "@azure/functions";
+
+const accountWithdraw: AzureFunction = async function (context: Context, timer: any): Promise<void> {
+  const timeStamp = new Date().toISOString();
+
+  if (timer.isPastDue) {
+    context.log("Timer function is running late!");
+  }
+  context.log("Timer trigger function ran!", timeStamp);
+};
+
+export default accountWithdraw;
